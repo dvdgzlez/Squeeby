6ccbed592f6100def146a758f7b5433e057ebcce
Jean Paul Mir Villamar <jeanpaul_mir@hotmail.com>
2019-03-22 18:29:09 -0400

    IdentityServer Integration

---
+++ Squeeby.Infrastructure/Configuration/ISqueebyConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Squeeby.Infrastructure.Configuration
+{
+    public interface ISqueebyConfiguration
+    {
+        string GoogleClientId { get; set; }
+        string GoogleSecret { get; set; }
+
+    }
+}
